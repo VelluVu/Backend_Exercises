776527a7b56a67a3efeac2180d4d3188d5f54d44
VelluVu <velluv@hotmail.fi>
2019-09-19 12:35:04 +0300

    update

---
+++ GameWebApi/GameWebApi/Controllers/PlayersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameWebApi.Players;
+using GameWebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GameWebApi.Controllers
+{
+    [Route ( "api/players/" )]
+    [ApiController]
+    public class PlayersController : Controller
+    {
+
+        private readonly IRepository repo;
+
+        public PlayersController ( IRepository repo )
+        {
+            this.repo = repo;
+        }
+
+        // GET: /<controller>/     
+        public IActionResult Index ( )
+        {
+            return View ( );
+        }
+
+        [HttpGet]
+        [Route ( "{id:guid}" )]
+        public Task<Player> Get ( Guid id )
+        {
+            return repo.Get ( id );
+        }
+
+        [HttpGet]
+        [Route ( "" )]
+        public Task<Player [ ]> GetAll ( )
+        {
+            return repo.GetAll ( );
+        }
+
+        [HttpPut]
+        [Route ( "{player:newplayer}" )]
+        public Task<Player> Create ( NewPlayer player )
+        {
+            Player newPlayer = new Player ( );
+            newPlayer.Id = new Guid ( );
+            newPlayer.Name = player.Name;
+            return repo.Create ( newPlayer );
+        }
+
+        [HttpPut]
+        [Route ( "{id:guid}{player:modifiedplayer}" )]
+        public Task<Player> Modify ( Guid id, ModifiedPlayer player )
+        {
+            return repo.Modify ( id, player );
+        }
+
+        [HttpDelete]
+        [Route ( "{id:guid}" )]
+        public Task<Player> Delete ( Guid id )
+        {
+            return repo.Delete ( id );
+        }
+    }
+}
---
+++ GameWebApi/GameWebApi/Repositories/FileRepository.cs
@@ -0,0 +1,141 @@
+﻿using GameWebApi.Players;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameWebApi.Repositories
+{
+    public class FileRepository : IRepository
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory + @"Repositories\game-dev.txt";
+
+        public async Task<Player> Get ( Guid id )
+        {
+
+            string [ ] textArray = await File.ReadAllLinesAsync ( path );
+            List<string> textList = textArray.ToList ( );
+
+            foreach ( var line in textList )
+            {
+                string [ ] entries = line.Split ( ',' );
+
+                if ( entries [ 0 ] == id.ToString ( ) )
+                {
+                    Player p = new Player ( );
+                    p.Id = new Guid ( entries [ 0 ] );
+                    p.Name = entries [ 1 ];
+                    p.Level = int.Parse ( entries [ 2 ] );
+                    p.Score = int.Parse ( entries [ 3 ] );
+                    p.IsBanned = bool.Parse ( entries [ 4 ] );
+
+                    return p;
+                }
+
+            }
+
+            throw new ArgumentException ( "Id cannot be found from the repository" );
+        }
+
+        public async Task<Player [ ]> GetAll ( )
+        {
+
+            List<Player> playerList = new List<Player> ( );
+
+            string [ ] textArray = await File.ReadAllLinesAsync ( path );
+            List<string> textList = textArray.ToList ( );
+
+            foreach ( var line in textList )
+            {
+                string [ ] entries = line.Split ( ',' );
+
+                Player p = await Get ( new Guid ( entries [ 0 ] ) );
+
+                playerList.Add ( p );
+            }
+
+
+            Player [ ] playerArray = playerList.ToArray ( );
+
+            return playerArray;
+        }
+
+        public async Task<Player> Create ( Player player )
+        {
+
+            string textLine =
+                player.Id + "," +
+                player.Name + "," +
+                player.Level + "," +
+                player.Score + "," +
+                player.IsBanned + ",";
+
+            //Writes new player to the txt file
+            await File.AppendAllTextAsync ( path, textLine );
+
+            using ( StreamWriter writer = new StreamWriter ( path, true ) ) //// true to append data to the file
+            {
+                writer.WriteLine ( textLine );
+            }
+
+            return player;
+
+        }
+
+        //Modifies player and rewrites the text file
+        public async Task<Player> Modify ( Guid id, ModifiedPlayer player )
+        {
+
+            Player [ ] players = await GetAll ( );
+            List<Player> playerList = players.ToList ( );
+
+            foreach ( var p in playerList )
+            {
+                if ( id == p.Id )
+                {
+                    p.Score = player.Score;
+
+                }
+            }
+
+            string [ ] textLines = new string [ playerList.Count ];
+
+            for ( int i = 0 ; i < textLines.Length ; i++ )
+            {
+                textLines [ i ] = playerList [ i ].Id.ToString ( ) + "," + playerList [ i ].Name + "," + playerList [ i ].Level.ToString ( ) + "," + playerList [ i ].Score.ToString ( ) + "," + playerList [ i ].IsBanned.ToString ( );
+            }
+
+            await File.WriteAllLinesAsync ( path, textLines );
+
+            return await Get ( id );
+        }
+
+        //Removes player from textfile and rewrites it
+        public async Task<Player> Delete ( Guid id )
+        {
+            Player [ ] players = await GetAll ( );
+            List<Player> playerList = players.ToList ( );
+
+            foreach ( var p in playerList )
+            {
+                if ( id == p.Id )
+                {
+                    playerList.Remove ( p );
+
+                }
+            }
+
+            string [ ] textLines = new string [ playerList.Count ];
+
+            for ( int i = 0 ; i < textLines.Length ; i++ )
+            {
+                textLines [ i ] = playerList [ i ].Id.ToString ( ) + "," + playerList [ i ].Name + "," + playerList [ i ].Level.ToString ( ) + "," + playerList [ i ].Score.ToString ( ) + "," + playerList [ i ].IsBanned.ToString ( );
+            }
+
+            await File.WriteAllLinesAsync ( path, textLines );
+
+            return await Get ( id );
+        }
+    }
+}
